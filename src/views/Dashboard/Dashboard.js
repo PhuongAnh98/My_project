1eb2be6be47f5cbad1e8568bfd2fc2d34654bd46
PhuongAnh98 <phuonganh280898@gmail.com>
2019-10-12 23:36:08 +0700

    fix dashboard

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -12,21 +12,37 @@ class Dashboard extends Component {
         <Row>
           <Col>
             <Card>
-              <h3>original image</h3>
-              <img style={{ width: "50%"}} src={img} />
-              <Col>
-                <Button>Pre</Button>
-                <Button>Next</Button>
-              </Col>
+
+              <img style={{ width: '100%', height: 'auto' }} src={img} />
+              <div>
+                <Button color="primary">Đẹp</Button>
+                <Button color="primary">Xấu</Button>
+                <Button color="primary">Bình thường</Button>
+              </div>
+              <div>
+                <Button >Prev</Button>
+                <Button >Next</Button>
+              </div>
             </Card>
 
           </Col>
           <Col >
-            <Card>
-              <h3>original image</h3>
-              <img style={{ width: "50%" }} src={img} />
-              <Button>Pre</Button>
-              <Button>Next</Button>
+            <Card>  
+              <img style={{ width: "50%", height: 'auto' }} src={img} />
+              <div  class="container" >
+                <Button color="primary">1</Button>
+                <Button color="primary">2</Button>
+                <Button color="primary">3</Button>
+                <Button color="primary">4</Button>
+                <Button color="primary">5</Button>
+                <Button color="primary">Loại</Button>
+
+              </div>
+              <div>
+                <Button>Prev</Button>
+                <Button>Next</Button>
+              </div>
+
             </Card>
           </Col>
         </Row>
